8aae666b4f8abb83ecaf710e7ab724737b62392a
ryan brenon <rbrenon@gmail.com>
2022-12-02 15:36:54 -0500

    day 1

---
+++ day_1/calorie_counting.py
@@ -0,0 +1,29 @@
+
+
+def main():
+    with open("input.txt") as file:
+        raw_input = file.read().splitlines()
+
+    print(part_one(raw_input))
+
+
+def part_one(input_data):
+    elfs_cals: list[int] = []
+    calorie_count: int = 0
+
+    for calorie_line in input_data:
+        if calorie_line == '':
+            elfs_cals.append(calorie_count)
+            calorie_count = 0
+        else:
+            calorie_count += int(calorie_line)
+
+    p1 = max(elfs_cals)     # 70509
+    p2 = sum((sorted(elfs_cals, reverse=True))[0:3])    # 208567
+
+    return f"Part 1: {p1} " \
+           f"Part 2: {p2}"
+
+
+if __name__ == "__main__":
+    main()
